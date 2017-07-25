be6e2511166e3b8a887c4db1f3edfde65363e6c2
福園康弘 <moze478@yahoo.co.jp>
2017-07-25 16:26:16 +0900

    StepAsideUnityChanプロジェクト
    不要ファイルを削除

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+	// Unityちゃんのオブジェクト
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		// Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find ("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Unityちゃんが通り過ぎ、画面外に出たオブジェクトを破棄
+		if((this.unitychan.transform.position.z - this.transform.position.z) > 5.0f){
+			Destroy (this.gameObject);
+		}
+	}
+}
